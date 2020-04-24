532f707d181d84d719496c89c1c728ed10d7366e
batbrain9392 <batbrain9392@gmail.com>
2020-04-24 17:40:24 +0100

    converted to material

--- src/Components/CountryPicker/CountryPicker.jsx
+++ src/Components/CountryPicker/CountryPicker.jsx
@@ -1,21 +1,36 @@
 import React, { memo } from 'react'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
 
 const CountryPicker = ({ countries, onChange }) => {
   // console.log({ CountryPicker: countries })
+
   const onChangeHandler = (event) => {
     const value = event.target.value
     onChange(value)
   }
 
   return (
-    <select onChange={onChangeHandler}>
-      <option value=''>The World</option>
-      {countries.map((country) => (
-        <option value={country} key={country}>
-          {country}
-        </option>
-      ))}
-    </select>
+    <FormControl>
+      <InputLabel shrink htmlFor='location'>
+        Location
+      </InputLabel>
+      <Select
+        native
+        onChange={onChangeHandler}
+        inputProps={{
+          name: 'location',
+          id: 'location',
+        }}>
+        <option value=''>The World</option>
+        {countries.map((country) => (
+          <option value={country} key={country}>
+            {country}
+          </option>
+        ))}
+      </Select>
+    </FormControl>
   )
 }
 
