dd76f74e3f8f63b1b7eb4f344e5b73b4b0db7a11
batbrain9392 <batbrain9392@gmail.com>
2020-04-25 09:12:13 +0100

    cards sorted

---
+++ src/Components/Cards/Card/Card.jsx
@@ -0,0 +1,52 @@
+import React, { memo } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import CountUp from 'react-countup'
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 170,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+})
+
+export default memo(({ header, count, lastUpdate, children }) => {
+  const classes = useStyles()
+
+  return (
+    <Grid item xs={12} sm={4} md='auto'>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color='textSecondary'
+            gutterBottom>
+            {header}
+          </Typography>
+          <Typography variant='h5' component='h2'>
+            <CountUp end={count} separator=',' />
+          </Typography>
+          <Typography className={classes.pos} color='textSecondary'>
+            as of {new Date(lastUpdate).toLocaleDateString()}
+          </Typography>
+          <Typography variant='body2' component='p'>
+            {children}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  )
+})
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,13 +1,41 @@
 import React, { memo } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
+import Card from './Card/Card'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}))
 
 const Cards = ({ countryData }) => {
   // console.log({ Cards: countryData })
+  const classes = useStyles()
+  if (!countryData) return null
+
   return (
     <Box mb={3}>
-      <Grid container justify='center'>
-        <pre>{JSON.stringify(countryData, null, 2)}</pre>
+      <Grid container justify='center' className={classes.root} spacing={2}>
+        <Card
+          header='Infected'
+          count={countryData.confirmed}
+          lastUpdate={countryData.lastUpdate}>
+          Number of active cases of COVID-19.
+        </Card>
+        <Card
+          header='Recovered'
+          count={countryData.recovered}
+          lastUpdate={countryData.lastUpdate}>
+          Number of recoveries from COVID-19.
+        </Card>
+        <Card
+          header='Deaths'
+          count={countryData.deaths}
+          lastUpdate={countryData.lastUpdate}>
+          Number of deaths caused by COVID-19.
+        </Card>
       </Grid>
     </Box>
   )
