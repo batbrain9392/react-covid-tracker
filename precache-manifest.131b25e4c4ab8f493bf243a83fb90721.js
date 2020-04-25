4d5e6e074ccdd9b4170bc55a48aa5e466ec461ee
batbrain9392 <batbrain9392@users.noreply.github.com>
2020-04-25 06:50:16 +0000

    Deploying to gh-pages from  - 65e87418e58433515a03e0983aa4fa5ea38af702 🚀

---
+++ precache-manifest.131b25e4c4ab8f493bf243a83fb90721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d79677801e4a4f7f3a4bf2832e918e",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "96e21899753455d41d90",
+    "url": "/react-covid-tracker/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "ec2ed5d389f90d579e48",
+    "url": "/react-covid-tracker/static/js/2.d4658a1e.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/react-covid-tracker/static/js/2.d4658a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e21899753455d41d90",
+    "url": "/react-covid-tracker/static/js/main.100a9513.chunk.js"
+  },
+  {
+    "revision": "e69d67ab6b71686b8d94",
+    "url": "/react-covid-tracker/static/js/runtime-main.65dbcbcf.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid-tracker/static/media/image.d7265326.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-covid-tracker/precache-manifest.a4b4893e156398b66ed58b5cce68ae82.js"
+  "/react-covid-tracker/precache-manifest.131b25e4c4ab8f493bf243a83fb90721.js"
 );
 
 self.addEventListener('message', (event) => {
