7fe070b2d16e2a63269b81f368a3805fc0f3693b
Debmallya Bhattacharya <batbrain9392@mac.local>
2020-05-05 15:29:07 +0100

    using axios-mock-adapter

--- src/api/index.test.js
+++ src/api/index.test.js
@@ -1,11 +1,64 @@
 import { renderHook } from '@testing-library/react-hooks'
 import { useCountries, useCountryData, useDailyData } from './index'
-import axiosMock from 'axios'
 
-jest.mock('axios')
+const countries = [
+  { name: 'India', iso2: 'IN', iso3: 'IND' },
+  { name: 'Ireland', iso2: 'IE', iso3: 'IRL' },
+]
+const countryIndiaData = {
+  confirmed: {
+    value: 46476,
+    detail: 'https://covid19.mathdro.id/api/countries/India/confirmed',
+  },
+  recovered: {
+    value: 12849,
+    detail: 'https://covid19.mathdro.id/api/countries/India/recovered',
+  },
+  deaths: {
+    value: 1571,
+    detail: 'https://covid19.mathdro.id/api/countries/India/deaths',
+  },
+  lastUpdate: '2020-05-05T12:32:31.000Z',
+}
+const dailyData = [
+  {
+    totalConfirmed: 555,
+    mainlandChina: 548,
+    otherLocations: 7,
+    deltaConfirmed: 0,
+    totalRecovered: 0,
+    confirmed: { total: 555, china: 548, outsideChina: 7 },
+    deltaConfirmedDetail: { total: 0, china: 0, outsideChina: 0 },
+    deaths: { total: 17, china: 17, outsideChina: 0 },
+    recovered: { total: 0, china: 0, outsideChina: 0 },
+    active: 0,
+    deltaRecovered: 0,
+    incidentRate: 0.44821646978651847,
+    peopleTested: 0,
+    reportDate: '2020-01-22',
+  },
+  {
+    totalConfirmed: 654,
+    mainlandChina: 643,
+    otherLocations: 11,
+    deltaConfirmed: 99,
+    totalRecovered: 0,
+    confirmed: { total: 654, china: 643, outsideChina: 11 },
+    deltaConfirmedDetail: { total: 99, china: 95, outsideChina: 4 },
+    deaths: { total: 18, china: 18, outsideChina: 0 },
+    recovered: { total: 0, china: 0, outsideChina: 0 },
+    active: 0,
+    deltaRecovered: 0,
+    incidentRate: 0.47555995744189605,
+    peopleTested: 0,
+    reportDate: '2020-01-23',
+  },
+]
+mock.onGet('/countries').reply(200, { countries })
+mock.onGet('/countries/India').reply(200, countryIndiaData)
+mock.onGet('/daily').reply(200, dailyData)
 
-async function setup(mockData, customHook, customHookParam) {
-  axiosMock.get.mockResolvedValueOnce({ data: mockData })
+async function setup(customHook, customHookParam) {
   const { result, waitForNextUpdate } = renderHook(() =>
     customHook(customHookParam)
   )
@@ -16,37 +69,14 @@ async function setup(mockData, customHook, customHookParam) {
 }
 
 test('useCountries hook should return a string array of country names', async () => {
-  const mockCountryList = {
-    countries: [
-      { name: 'India', iso2: 'IN', iso3: 'IND' },
-      { name: 'Ireland', iso2: 'IE', iso3: 'IRL' },
-    ],
-  }
-  const result = await setup(mockCountryList, useCountries)
+  const result = await setup(useCountries)
 
-  expect(axiosMock.get).toHaveBeenLastCalledWith('/countries')
   expect(result).toEqual(['India', 'Ireland'])
 })
 
 test('useCountryData hook should return data for the provided country', async () => {
-  const mockCountryData = {
-    confirmed: {
-      value: 46476,
-      detail: 'https://covid19.mathdro.id/api/countries/India/confirmed',
-    },
-    recovered: {
-      value: 12849,
-      detail: 'https://covid19.mathdro.id/api/countries/India/recovered',
-    },
-    deaths: {
-      value: 1571,
-      detail: 'https://covid19.mathdro.id/api/countries/India/deaths',
-    },
-    lastUpdate: '2020-05-05T12:32:31.000Z',
-  }
-  const result = await setup(mockCountryData, useCountryData, 'India')
+  const result = await setup(useCountryData, 'India')
 
-  expect(axiosMock.get).toHaveBeenLastCalledWith('/countries/India')
   expect(result).toEqual({
     country: 'India',
     confirmed: 46476,
@@ -57,42 +87,7 @@ test('useCountryData hook should return data for the provided country', async ()
 })
 
 test('useDailyData hook should return an object array of daily data', async () => {
-  const mockDailyData = [
-    {
-      totalConfirmed: 555,
-      mainlandChina: 548,
-      otherLocations: 7,
-      deltaConfirmed: 0,
-      totalRecovered: 0,
-      confirmed: { total: 555, china: 548, outsideChina: 7 },
-      deltaConfirmedDetail: { total: 0, china: 0, outsideChina: 0 },
-      deaths: { total: 17, china: 17, outsideChina: 0 },
-      recovered: { total: 0, china: 0, outsideChina: 0 },
-      active: 0,
-      deltaRecovered: 0,
-      incidentRate: 0.44821646978651847,
-      peopleTested: 0,
-      reportDate: '2020-01-22',
-    },
-    {
-      totalConfirmed: 654,
-      mainlandChina: 643,
-      otherLocations: 11,
-      deltaConfirmed: 99,
-      totalRecovered: 0,
-      confirmed: { total: 654, china: 643, outsideChina: 11 },
-      deltaConfirmedDetail: { total: 99, china: 95, outsideChina: 4 },
-      deaths: { total: 18, china: 18, outsideChina: 0 },
-      recovered: { total: 0, china: 0, outsideChina: 0 },
-      active: 0,
-      deltaRecovered: 0,
-      incidentRate: 0.47555995744189605,
-      peopleTested: 0,
-      reportDate: '2020-01-23',
-    },
-  ]
-  const result = await setup(mockDailyData, useDailyData)
+  const result = await setup(useDailyData)
 
-  expect(axiosMock.get).toHaveBeenLastCalledWith('/daily')
-  expect(result).toEqual(mockDailyData)
+  expect(result).toEqual(dailyData)
 })
