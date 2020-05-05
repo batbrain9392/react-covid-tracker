f2702d05344d2046c8e08e19d1442b7773db2ded
Debmallya Bhattacharya <batbrain9392@mac.local>
2020-05-05 15:38:33 +0100

    refactoring

--- src/api/index.test.js
+++ src/api/index.test.js
@@ -1,62 +1,13 @@
 import { renderHook } from '@testing-library/react-hooks'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import mockData from './mockData'
 import { useCountries, useCountryData, useDailyData } from './index'
 
-const countries = [
-  { name: 'India', iso2: 'IN', iso3: 'IND' },
-  { name: 'Ireland', iso2: 'IE', iso3: 'IRL' },
-]
-const countryIndiaData = {
-  confirmed: {
-    value: 46476,
-    detail: 'https://covid19.mathdro.id/api/countries/India/confirmed',
-  },
-  recovered: {
-    value: 12849,
-    detail: 'https://covid19.mathdro.id/api/countries/India/recovered',
-  },
-  deaths: {
-    value: 1571,
-    detail: 'https://covid19.mathdro.id/api/countries/India/deaths',
-  },
-  lastUpdate: '2020-05-05T12:32:31.000Z',
-}
-const dailyData = [
-  {
-    totalConfirmed: 555,
-    mainlandChina: 548,
-    otherLocations: 7,
-    deltaConfirmed: 0,
-    totalRecovered: 0,
-    confirmed: { total: 555, china: 548, outsideChina: 7 },
-    deltaConfirmedDetail: { total: 0, china: 0, outsideChina: 0 },
-    deaths: { total: 17, china: 17, outsideChina: 0 },
-    recovered: { total: 0, china: 0, outsideChina: 0 },
-    active: 0,
-    deltaRecovered: 0,
-    incidentRate: 0.44821646978651847,
-    peopleTested: 0,
-    reportDate: '2020-01-22',
-  },
-  {
-    totalConfirmed: 654,
-    mainlandChina: 643,
-    otherLocations: 11,
-    deltaConfirmed: 99,
-    totalRecovered: 0,
-    confirmed: { total: 654, china: 643, outsideChina: 11 },
-    deltaConfirmedDetail: { total: 99, china: 95, outsideChina: 4 },
-    deaths: { total: 18, china: 18, outsideChina: 0 },
-    recovered: { total: 0, china: 0, outsideChina: 0 },
-    active: 0,
-    deltaRecovered: 0,
-    incidentRate: 0.47555995744189605,
-    peopleTested: 0,
-    reportDate: '2020-01-23',
-  },
-]
-mock.onGet('/countries').reply(200, { countries })
-mock.onGet('/countries/India').reply(200, countryIndiaData)
-mock.onGet('/daily').reply(200, dailyData)
+const mock = new MockAdapter(axios)
+mock.onGet('/countries').reply(200, { countries: mockData.countries })
+mock.onGet('/countries/India').reply(200, mockData.countryIndiaData)
+mock.onGet('/daily').reply(200, mockData.dailyData)
 
 async function setup(customHook, customHookParam) {
   const { result, waitForNextUpdate } = renderHook(() =>
@@ -89,5 +40,5 @@ test('useCountryData hook should return data for the provided country', async ()
 test('useDailyData hook should return an object array of daily data', async () => {
   const result = await setup(useDailyData)
 
-  expect(result).toEqual(dailyData)
+  expect(result).toEqual(mockData.dailyData)
 })
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,7 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
-import axios from 'axios'
-import MockAdapter from 'axios-mock-adapter'
-
-global.mock = new MockAdapter(axios)
